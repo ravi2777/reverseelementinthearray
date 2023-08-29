5972a6d650e885d1fd993568781afc32c007832e
hp <hp@RAVIBAGHEL>
2023-08-29 22:13:41 +0530

    Add project files.

---
+++ reverseelementinthearray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        int length = array.Length;
+
+        for (int i = 0; i < length / 2; i++)
+        {
+            int temp = array[i];
+            array[i] = array[length - 1 - i];
+            array[length - 1 - i] = temp;
+        }
+
+        Console.WriteLine("Reversed array: " + string.Join(", ", array));
+    }
+}
